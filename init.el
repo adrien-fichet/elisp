2a6ac08339ec60f8276699ed95808f77117d2882
Adrien <adrien.fichet@gmail.com>
2011-09-18 17:21:34 +0200

    Add fullscreen feature, activated by default & switchable with F11.

--- init.el
+++ init.el
@@ -177,3 +177,14 @@
 (setq initial-scratch-message ";;                _\n;; __      ____ _| |_\n;; \\ \\ /\\ / / _` | __|\n;;  \\ V  V / (_| | |_\n;;   \\_/\\_/ \\__,_|\\__|\n;;\n\n")
 (setq inhibit-startup-screen t)
 
+;; Fullscreen avec F11
+(defun toggle-fullscreen (&optional f)
+  (interactive)
+  (let ((current-value (frame-parameter nil 'fullscreen)))
+    (set-frame-parameter nil 'fullscreen
+                         (if (equal 'fullboth current-value)
+                             (if (boundp 'old-fullscreen) old-fullscreen nil)
+                           (progn (setq old-fullscreen current-value)
+                                  'fullboth)))))
+(global-set-key [f11] 'toggle-fullscreen)
+(toggle-fullscreen)
